d02f64f2b7e170c287bb4118d99e9d2bf79470d3
Anthony S <43185562+anchovey707@users.noreply.github.com>
2018-11-03 15:15:17 -0400

    Delete DbAccessConfiguartion.java

--- AirplaneTicketSystem/src/project/db/DbAccessConfiguartion.java
+++
@@ -1,7 +0,0 @@
-package project.db;
-	//this class is done
-public abstract class DbAccessConfiguartion{
-	static String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/airplanetickets";
-	static String DB_CONNECTION_USERNAME = "root";
-	static String DB_CONNECTION_PASSWORD = "aannt707";
-}
