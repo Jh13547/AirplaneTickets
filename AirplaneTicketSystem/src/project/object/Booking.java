903615db8aec804f46041225e0f408a6a11bd4d5
Jamie Hayes <Jdhay@10.40.15.98>
2018-11-10 19:36:02 -0500

    Finalized logic and models this all should be ready for integration now works with the database.

--- AirplaneTicketSystem/src/project/object/Booking.java
+++ AirplaneTicketSystem/src/project/object/Booking.java
@@ -2,4 +2,27 @@ package project.object;
 
 public class Booking {
 //TODO set up the logic for the booking model
+	int bookingid, uid, flightid;
+	double amountbooked;
+	public Booking(int bookingid, int uid, int flightid, double amountbooked) {
+		this.bookingid = bookingid;
+		this.uid = uid;
+		this.flightid = flightid;
+		this.amountbooked = amountbooked;
+		
+	}
+	public int getBookingid() {
+		return bookingid;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public int getFlightid() {
+		return flightid;
+	}
+	public double getAmountbooked() {
+		return amountbooked;
+	}
+	
+	
 }
